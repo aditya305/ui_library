95c6cf74f85219fa8d621f45abc34b8ae7a2b01f
Aditya Sutar <aditya.sutar305@gmail.com>
2023-10-27 19:54:38 +0530

    Update router.dart

--- lib/router.dart
+++ lib/router.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:ui_library/main_page.dart';
 import 'package:ui_library/ui/ui.dart';
 
 class AppRouter {
   static const String home = '/';
+  static const String notesApp = "/sample-one";
   static const String createNote = '/createNote';
 
   static Route<dynamic>? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case home:
+        return MaterialPageRoute(
+          builder: (_) => MainPage(),
+        );
+      case notesApp:
         return MaterialPageRoute(
           builder: (_) => SampleOne(),
         );
